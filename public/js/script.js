5af0873ba15322c0a3df88a20e91eef79bcb1b60
William Lim <manvinedi@gmail.com>
2018-02-20 02:05:41 +0800

    updated checkbox limits

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
-{/* <div class="pricing-levels-3">
-  <p><strong>Which level would you like? (Select 3 Levels)</strong></p>
-  <input class="single-checkbox"type="checkbox" name="vehicle" value="Bike">Level 1<br>
-    <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 2<br>
-      <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 3<br>
-        <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 4<br>
-          <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 5<br>
-            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 6<br>
-              <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 7<br>
-              </div> */}
+/* <div class="pricing-levels-3">
+<p><strong>Which level would you like? (Select 3 Levels)</strong></p>
+<input class="single-checkbox"type="checkbox" name="vehicle" value="Bike">Level 1<br>
+<input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 2<br>
+<input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 3<br>
+<input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 4<br>
+<input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 5<br>
+<input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 6<br>
+<input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 7<br>
+</div> */
 
 var limit = 3;
 $('input.single-checkbox').on('change', function(evt) {
@@ -15,3 +15,5 @@ $('input.single-checkbox').on('change', function(evt) {
        this.checked = false;
    }
 });
+
+//https://stackoverflow.com/questions/19001844/how-to-limit-the-number-of-selected-checkboxes
